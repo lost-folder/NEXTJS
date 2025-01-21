4700e5a8edb7d2cd91cdcc0667a2e857dfc852db
lost-folder <lostfolder32@gmail.com>
2025-01-21 08:47:37 +0300

    end of the course

--- app/(dashboard)/tickets/[id]/DeleteIcon.js
+++ app/(dashboard)/tickets/[id]/DeleteIcon.js
@@ -1,45 +1,48 @@
 "use client"
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-
+// import { useState } from 'react'
+// import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { deleteTicket } from '../actions'
 
 // icons & UI
 // import { TiDelete } from 'react-icons/ti'
 
 export default function DeleteIcon({ id }) {
-  const [isLoading, setIsLoading] = useState(false)
+  let [isPending, startTransition] = useTransition()
 
-  const handleClick = async () => {
-    setIsLoading(true)
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-      method: 'DELETE'
-    })
-    const json = await res.json()
+  // const [isLoading, setIsLoading] = useState(false)
 
-    if (json.error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-    if (!json.error) {
-      router.refresh()
-      router.push('/tickets')
-    }
-  }
+  // const handleClick = async () => {
+  //   setIsLoading(true)
+  //   const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //   const json = await res.json()
+
+  //   if (json.error) {
+  //     console.log(error)
+  //     setIsLoading(false)
+  //   }
+  //   if (!json.error) {
+  //     router.refresh()
+  //     router.push('/tickets')
+  //   }
+  // }
   
 
   return (
     <button 
       className="btn-primary" 
-      onClick={handleClick}
-      disabled={isLoading}
+      onClick={() => startTransition(() => deleteTicket(id))}
+      disabled={isPending }
     >
-      {isLoading && (
+      {isPending  && (
         <>
           
           Deleting....
         </>
       )}
-      {!isLoading && (
+      {!isPending  && (
         <>
           Delete Ticket
         </>
